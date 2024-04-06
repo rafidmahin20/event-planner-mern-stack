bb143ad219f0c951798b09db4684e05e13394343
rafidmahin20 <submanmahin@gmail.com>
2024-04-06 15:24:03 +0600

    created

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {dbConnection} from "./database/dbConnection.js";
 import dotenv from "dotenv";
+import messageRouter from "./router/messageRouter.js";
 
 const app = express();
 
@@ -8,6 +9,8 @@ dotenv.config({path: ".env"});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use("/api/v1/message", messageRouter);
 
 dbConnection()
 
