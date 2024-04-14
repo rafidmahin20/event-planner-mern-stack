6e8b45d0a1fecf0d9be1d032deec854d5d6e6fb4
rafidmahin20 <submanmahin@gmail.com>
2024-04-14 21:40:52 +0600

    navbar styling

--- client/event-planner-frontend/src/Components/Navbar.jsx
+++ client/event-planner-frontend/src/Components/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useState } from 'react';
+import {Link} from "react-scroll";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='logo'>EVENT PLANNER</div>
+      <div className={show ? "navLinks showmenu" : "navLinks"}>
+        <div className='links'>
+          <Link to="landing" spy={true} duration={500}>HOME</Link>
+          <Link to="serviecs" spy={true} duration={500}>SERVICES</Link>
+          <Link to="about" spy={true} duration={500}>ABOUT</Link>
+          <Link to="contact" spy={true} duration={500}>CONTACT</Link>
+        </div>
+      </div>
+    </nav>
   )
 }
 
