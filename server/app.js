bc7a2e1a39a5f7334d2562c76720c41982ed4194
rafidmahin20 <submanmahin@gmail.com>
2024-04-14 21:09:08 +0600

    done backend

--- server/app.js
+++ server/app.js
@@ -2,10 +2,17 @@ import express from "express";
 import {dbConnection} from "./database/dbConnection.js";
 import dotenv from "dotenv";
 import messageRouter from "./router/messageRouter.js";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config({path: ".env"});
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
+    credentials: true,
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
