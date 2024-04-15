394942000b7cb8302510abcd06d3222539d5c022
rafidmahin20 <submanmahin@gmail.com>
2024-04-15 21:01:39 +0600

    hero section done

--- client/event-planner-frontend/src/Components/HeroSection.jsx
+++ client/event-planner-frontend/src/Components/HeroSection.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Link } from 'react-scroll';
+import Event1 from "../images/event1.jpg";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <section className='hero'>
+      <img src={Event1} alt='events'/>
+      <div className='item'>
+        <h1>Dream Maker</h1>
+        <div>
+          <h1>Your personal dream maker</h1>
+          <p>
+            We believe that it is all about the BIG DREAMS and the small
+          </p>
+          <Link to="contact" spy={true} duration={500}>
+            Book Now
+          </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- client/event-planner-frontend/src/Components/Navbar.jsx
+++ client/event-planner-frontend/src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import {Link} from "react-scroll";
+import {GiHamburgerMenu} from "react-icons/gi";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
@@ -8,11 +9,14 @@ const Navbar = () => {
       <div className='logo'>EVENT PLANNER</div>
       <div className={show ? "navLinks showmenu" : "navLinks"}>
         <div className='links'>
-          <Link to="landing" spy={true} duration={500}>HOME</Link>
+          <Link to="hero" spy={true} duration={500}>HOME</Link>
           <Link to="serviecs" spy={true} duration={500}>SERVICES</Link>
           <Link to="about" spy={true} duration={500}>ABOUT</Link>
           <Link to="contact" spy={true} duration={500}>CONTACT</Link>
         </div>
+      </div>
+      <div className="hamburger" onClick={() => setShow(!show)}>
+        <GiHamburgerMenu/>
       </div>
     </nav>
   )
