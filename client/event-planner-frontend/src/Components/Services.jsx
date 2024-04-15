c95517c54edce017206b02531bff773edd034ce5
rafidmahin20 <submanmahin@gmail.com>
2024-04-15 21:15:39 +0600

    edited

--- client/event-planner-frontend/src/Components/Services.jsx
+++ client/event-planner-frontend/src/Components/Services.jsx
@@ -1,6 +1,38 @@
 import React from 'react'
 
 const Services = () => {
+  const services = [
+    {
+      id: 1,
+      url: "",
+      title: "Birthday Planning",
+    },
+    {
+      id: 2,
+      url: "",
+      title: "Anniversary Planning",
+    },
+    {
+      id: 3,
+      url: "",
+      title: "Camping Trip Planning",
+    },
+    {
+      id: 1,
+      url: "",
+      title: "Birthday Planning",
+    },
+    {
+      id: 1,
+      url: "",
+      title: "Birthday Planning",
+    },
+    {
+      id: 1,
+      url: "",
+      title: "Birthday Planning",
+    },
+  ]
   return (
     <div>Services</div>
   )
