977cec3bcb69abbfcb91b4141fa8c9b19b53349e
rafidmahin20 <submanmahin@gmail.com>
2024-04-19 16:17:46 +0600

    frontend done

--- client/event-planner-frontend/src/Components/Contact.jsx
+++ client/event-planner-frontend/src/Components/Contact.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import { useState } from 'react'
+import toast from 'react-hot-toast';
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    await axios.post(
+      "http://localhost:4000/api/v1/message/send", {
+        name, email, subject, message
+      }, {
+        withCredentials: true, headers: {"Content-Type": "application/json"}
+      }
+    ).then(res => {
+      toast.success(res.data.message);
+      setName("");
+      setEmail("");
+      setSubject("");
+      setMessage("");
+    }).catch((error) => {
+      toast.error(error.response.data.message);
+    });
+  };
   return (
     <div>
       <div className='contact container'>
@@ -22,7 +47,18 @@ const Contact = () => {
           <div className="item">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29206.49586209974!2d90.4104344!3d23.7897087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7a0f70deb73%3A0x30c36498f90fe23!2sGulshan%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1713507274696!5m2!1sen!2sbd"  style={{width:"600%", height:"450px", border:0}}allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
-          <div className="item"></div>
+          <div className="item">
+            <form onSubmit={handleSendMessage}>
+              <h2>CONTACT</h2>
+              <div>
+                <input type='text' placeholder='name' value={name} onChange={(e) => setName(e.target.name)}/>
+                <input type='email' placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.email)}/>
+              </div>
+              <input type='text' placeholder='subject' value={subject} onChange={(e) => setSubject(e.target.subject)}/>
+              <textarea rows={10} placeholder='message' value={message} onChange={(e) => setMessage(e.target.message)}/>
+              <button type='submit'>Send</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
