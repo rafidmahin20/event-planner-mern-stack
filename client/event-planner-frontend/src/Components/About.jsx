968f09999f166f3aa62017efd573c29d2898332f
rafidmahin20 <submanmahin@gmail.com>
2024-04-19 11:56:03 +0600

    abut section completed

--- client/event-planner-frontend/src/Components/About.jsx
+++ client/event-planner-frontend/src/Components/About.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='about container'>
+      <h4>Events</h4>
+      <h2>About</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi asperiores similique cumque rerum vel earum, fugiat ducimus dolor nostrum, iure eos, quas reprehenderit ex delectus aliquam! Eos optio numquam architecto molestias exercitationem odio. Ad adipisci dolores beatae ex, animi repudiandae natus velit, incidunt quisquam laboriosam esse voluptas deserunt vitae cum a. Suscipit ducimus dolorem eos aliquid doloremque magni, totam deleniti, magnam eius necessitatibus sed aut ea? Harum exercitationem culpa reprehenderit aliquid cumque amet, voluptatem nostrum impedit rem consequatur dolor eum sapiente. Dicta, iure facilis. Nam voluptatum provident a! Animi eveniet vel asperiores odio tempora, error tempore eius nihil ratione similique!</p>
+    </section>
   )
 }
 
